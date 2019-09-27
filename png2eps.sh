e04c34a61162ac22cd87d9864847534e74bd4918
Vasilii <pushkarev.vv14@physics.msu.ru>
2019-09-28 01:53:33 +0300

    Update png2eps.sh

--- png2eps.sh
+++ png2eps.sh
@@ -7,14 +7,12 @@
 #####################################################
 
 cd $1
-res_dir='./result'
-if [ ! -d $res_dir ]; then mkdir $res_dir; fi
 
 for file in $(find './' -name '*.png');
 do
 	#new name creating
 	name=$( echo $file | cut -d "." -f 2 )
-	out=$res_dir'/'$( echo ${name##*/} )'.eps'
+	out='.'$name'.eps'
 	
 	#converting
 	convert $file $out
